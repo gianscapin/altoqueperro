6c97a88ba51380b4e2c2c4f0b5a1638c972c6662
Erik Meisel <erikmeisel@gmail.com>
2021-09-19 13:59:27 -0300

    vets

---
+++ app/src/main/java/com/ort/altoqueperro/entities/Vet.kt
@@ -0,0 +1,20 @@
+package com.ort.altoqueperro.entities
+
+import android.media.Image
+
+class Vet(
+    id:String,
+    name:String,
+    latitude:Double,
+    longitude:Double,
+    neighborhood:String,
+    phone:String,
+    street:String,
+    streetNumber:String,
+    description: String?,
+    businessHours:String,
+    logo: Image?,
+    city:String,
+    country:String
+) {
+}
---
+++ app/src/main/java/com/ort/altoqueperro/entities/VetRepository.kt
@@ -0,0 +1,40 @@
+package com.ort.altoqueperro.entities
+
+class VetRepository {
+    fun getAllVets(): MutableList<Vet> {
+
+        return mutableListOf(
+            Vet(
+                "v1",
+                "Hers Pet",
+                -34.6089609,
+                -58.4370584,
+                "Caballito",
+                "11 4477-3486",
+                "Av. Díaz Vélez",
+                "4342",
+                null,
+                "L a L 10 a 16",
+                null,
+                "Buenos Aires",
+                "Argentina"
+            ),
+            Vet(
+                "v2",
+                "VETERINARIA CENTRO MÉDICO DELFI'S",
+                -34.626831,
+                -58.4362001,
+                "Caballito",
+                "11 4922-5520",
+                "Av. José María Moreno 515",
+                "515",
+                null,
+                "L a V 10 a 20",
+                null,
+                "Buenos Aires",
+                "Argentina"
+            )
+
+        );
+    }
+}
