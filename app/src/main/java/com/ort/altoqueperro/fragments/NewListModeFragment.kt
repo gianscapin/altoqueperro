4beca80024b3b0a5887467c9f4a79e803ef35387
Ezequiel Gresia <egresia@onapsis.com>
2021-09-26 16:10:36 -0300

    new redirect bottom nav bar

--- app/src/main/java/com/ort/altoqueperro/fragments/NewListModeFragment.kt
+++ app/src/main/java/com/ort/altoqueperro/fragments/NewListModeFragment.kt
@@ -7,26 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ort.altoqueperro.R
-import com.ort.altoqueperro.viewmodels.SettingsViewModel
+import com.ort.altoqueperro.viewmodels.NewListModeViewModel
 
-class Settings : Fragment() {
+class NewListModeFragment : Fragment() {
 
     companion object {
-        fun newInstance() = Settings()
+        fun newInstance() = NewListModeFragment()
     }
 
-    private lateinit var viewModel: SettingsViewModel
+    private lateinit var viewModel: NewListModeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+        return inflater.inflate(R.layout.new_list_mode_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(NewListModeViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
---
+++ app/src/main/java/com/ort/altoqueperro/viewmodels/NewProfileUserViewModel.kt
@@ -0,0 +1,7 @@
+package com.ort.altoqueperro.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class NewProfileUserViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
