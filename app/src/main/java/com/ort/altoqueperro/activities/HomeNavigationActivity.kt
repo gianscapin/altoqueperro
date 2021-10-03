23a38b874312840a52b42c6e82d34f636a42fa95
Juan I. Bartamian <juan.i.bartamian@gmail.com>
2021-10-03 20:47:25 -0300

    back arrow removed

--- app/src/main/java/com/ort/altoqueperro/activities/HomeNavigationActivity.kt
+++ app/src/main/java/com/ort/altoqueperro/activities/HomeNavigationActivity.kt
@@ -37,7 +37,7 @@ class HomeNavigationActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.newMapModeFragment,R.id.shelterItemFragment,R.id.newProfileUserFragment, R.id.settings)
+            setOf(R.id.newMapModeFragment,R.id.shelterListFragment, R.id.vetListFragment,R.id.newProfileUserFragment, R.id.settings, R.id.lostPetListFragment)
         )
 
         val floatingButtonAction:View = findViewById(R.id.btnFloatingActions)
