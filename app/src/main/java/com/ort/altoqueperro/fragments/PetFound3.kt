1e3f61b9f46ba862d8a60966b8e4af446f1687ad
Ezequiel Gresia <egresia@onapsis.com>
2021-11-11 21:06:20 -0300

    adding calendar to request

--- app/src/main/java/com/ort/altoqueperro/fragments/PetFound3.kt
+++ app/src/main/java/com/ort/altoqueperro/fragments/PetFound3.kt
@@ -1,5 +1,8 @@
 package com.ort.altoqueperro.fragments
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,7 +11,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
@@ -29,6 +34,7 @@ class PetFound3 : Fragment() {
     lateinit var rootLayout : ConstraintLayout
     private val viewModel: PetFoundViewModel by activityViewModels()
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,13 +55,31 @@ class PetFound3 : Fragment() {
         })
 
         date = v.findViewById(R.id.txtDate)
-        date.addTextChangedListener(object : TextWatcher {
+        /*date.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 viewModel.setLostDate(s.toString())
             }
             override fun afterTextChanged(s: Editable?) {}
-        })
+        })*/
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        date.setOnClickListener {
+            val dpd = DatePickerDialog(
+                requireContext(),
+                DatePickerDialog.OnDateSetListener { view: DatePicker, mYear: Int, mMonth: Int, mDay: Int ->
+                    date.setText("" + mDay + "/" + mMonth + "/" + mYear)
+                },
+                year,
+                month,
+                day
+            )
+            dpd.show()
+        }
 
 
         return v
