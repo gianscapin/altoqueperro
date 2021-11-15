3cfc217267a1ea1a0aa6c79b7fd79e85a9c9c67f
nicoRoldan128 <nicolasroldan128@gmail.com>
2021-11-14 22:55:22 -0300

    Agrego_BtnWhatsapp_contactDueño

--- app/src/main/java/com/ort/altoqueperro/fragments/LostPetItemFragment.kt
+++ app/src/main/java/com/ort/altoqueperro/fragments/LostPetItemFragment.kt
@@ -1,5 +1,7 @@
 package com.ort.altoqueperro.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +13,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ort.altoqueperro.R
+import com.ort.altoqueperro.activities.MainActivity
 import com.ort.altoqueperro.entities.FoundPetRequest
 import com.ort.altoqueperro.entities.LostPetRequest
 import com.ort.altoqueperro.entities.PetRequest
+import com.ort.altoqueperro.entities.User
 import com.ort.altoqueperro.viewmodels.LostPetItemViewModel
+import java.lang.Exception
 
 class LostPetItemFragment : Fragment() {
     lateinit var v: View
@@ -30,7 +39,10 @@ class LostPetItemFragment : Fragment() {
     private lateinit var lostPetFurColor: TextView
     private lateinit var lostPetComments: TextView
     private lateinit var lostPetLostDate: TextView
+    private lateinit var btnContactWhatsapp: TextView
     private lateinit var lostPetData: LostPetRequest //ToDo cambiar las variables a foundpetrequest
+    private lateinit var userId: String
+    private lateinit var user: User
 
 
     companion object {
@@ -55,14 +67,15 @@ class LostPetItemFragment : Fragment() {
         lostPetFurColor = v.findViewById(R.id.txtPetFurColor)
         lostPetComments = v.findViewById(R.id.txtPetComments)
         lostPetLostDate = v.findViewById(R.id.txtPetLostDate)
+        btnContactWhatsapp = v.findViewById(R.id.btnWP)
 
         return v
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(LostPetItemViewModel::class.java)
+        userId = LostPetItemFragmentArgs.fromBundle(requireArguments()).petData?.requestCreator.toString()
         lostPetData = LostPetItemFragmentArgs.fromBundle(requireArguments()).petData!!
         lostPetName.text = lostPetData.pet.name
         lostPetType.text = lostPetData.pet.type
@@ -75,5 +88,30 @@ class LostPetItemFragment : Fragment() {
         lostPetLostDate.text = lostPetData.pet.lostDate
         Glide.with(view).load(lostPetData.imageURL).into(lostPetImage)
 
+        viewModel.getUser(userId)
+
+        viewModel.userLiveData.observe(viewLifecycleOwner, {
+            user = it
+        })
+
+        btnContactWhatsapp.setOnClickListener {
+            sendWhatsApp()
+        }
+
     }
+
+    fun sendWhatsApp() {
+        try {
+            val text = "Hola! ${user.name} Te contacto gracias a Al Toque Perro, me parece que encontraste a mi mascota!"
+            val toNumber = user.phone
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("http://api.whatsapp.com/send?phone=$toNumber&text=$text")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/ort/altoqueperro/viewmodels/LostPetItemViewModel.kt
+++ app/src/main/java/com/ort/altoqueperro/viewmodels/LostPetItemViewModel.kt
@@ -1,7 +1,15 @@
 package com.ort.altoqueperro.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ort.altoqueperro.entities.User
+import com.ort.altoqueperro.repos.UserRepository
 
 class LostPetItemViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var userLiveData: MutableLiveData<User> = MutableLiveData()
+
+    fun getUser(userId: String) {
+        UserRepository().getUser(userLiveData, userId)
+    }
 }
